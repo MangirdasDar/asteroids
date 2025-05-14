dbd8fe6d1d6dc0852fa5b2ca277f1674fa683111
MangirdasDar <168616948+MangirdasDar@users.noreply.github.com>
2025-05-14 14:56:04 +0300

    Updated main.py with solution files

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            pygame.Surface.fill(screen, (0,0,0))
-            pygame.display.flip()
-
+            
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
